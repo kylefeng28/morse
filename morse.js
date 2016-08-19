85f43c53fb0150158b746be576f13f93cfc38ece
Kyle Feng <kylefeng28@gmail.com>
2016-08-19 00:24:55 -0400

    Add encoder, decoder, sound

---
+++ morse.js
@@ -0,0 +1,160 @@
+"use strict";
+
+var Morse = Morse || {};
+
+/* Constants */
+// 1 DAH = 3 DIT
+Morse.DIT = 60;
+Morse.DAH = 180;
+
+Morse.encTab = {
+	'A': '.-',
+	'B': '-...',
+	'C': '-.-.',
+	'D': '-..',
+	'E': '.',
+	'F': '..-.',
+	'G': '--.',
+	'H': '....',
+	'I': '..',
+	'J': '.---',
+	'K': '-.-',
+	'L': '.-..',
+	'M': '--',
+	'N': '-.',
+	'O': '---',
+	'P': '.--.',
+	'Q': '--.-',
+	'R': '.-.',
+	'S': '...',
+	'T': '-',
+	'U': '..-',
+	'V': '...-',
+	'W': '.--',
+	'X': '-..-',
+	'Y': '-.--',
+	'Z': '--..',
+	'0': '-----',
+	'1': '.----',
+	'2': '..---',
+	'3': '...--',
+	'4': '....-',
+	'5': '.....',
+	'6': '-....',
+	'7': '--...',
+	'8': '---..',
+	'9': '----.',
+	',': '--..--',
+	'.': '.-.-.-',
+	'?': '..--..',
+	';': '-.-.-.',
+	':': '---...',
+	"'": '.----.',
+	'-': '-....-',
+	'/': '-..-.',
+	'(': '-.--.-',
+	')': '-.--.-',
+	'_': '..--.-',
+	' ': ' ',
+
+	// prosigns
+	'SOS': '...---...',
+};
+
+// Flip dictionary
+Morse.decTab = {};
+(function() {
+for (var key in Morse.encTab) {
+	if (Morse.encTab.hasOwnProperty(key))
+		Morse.decTab[Morse.encTab[key]] = key;
+}
+})();
+
+Morse.encode = function(str) {
+	var m = [];
+	Array.prototype.map.call(str.toUpperCase(), function(x) { m.push(Morse.encTab[x]); });
+	return m;
+};
+
+Morse.decode = function(arr) {
+	var m = [];
+	Array.prototype.map.call(arr, function(x) { m.push(Morse.decTab[x]); });
+	return m.join(' ');
+};
+
+// Returns array of [onDuration, offDuration]
+Morse.getDurations = function(arr) {
+	var durs = [];
+	Array.prototype.map.call(arr, function(x) { // call() is needed bc `arr` may not be a true Array
+		if (x == '.') durs.push(Morse.DIT, Morse.DIT); else
+		if (x == '-') durs.push(Morse.DAH, Morse.DIT); else
+		              durs.push(0, Morse.DAH);
+	});
+	return durs;
+};
+
+// Returns bitstream
+// TODO don't depend on getDurations
+Morse.getBitstream = function(arr) {
+	var durs = Morse.getDurations(arr);
+	var bits = [];
+	durs.map(function(x, i) {
+		var on = (i+1) % 2; // odd elements are onDurs, even elements are offDurs (natural counting)
+		for (var j = 0; j < (x / 10); j++) { // multiply by a factor to make faster
+			bits.push(on);
+		}
+	});
+	return bits;
+};
+
+Morse.vibrate = function(arr) {
+	var durs = Morse.getDurations(arr);
+	window.navigator.vibrate(durs);
+};
+
+/* Signal */
+// From http://stackoverflow.com/a/16573282
+Morse.signalInit = function() {
+	// Init sine oscillator
+	var context = new (window.AudioContext || window.webkitAudioContext)();
+	Morse.vol = context.createGain();
+
+	Morse.vol.gain.value = 0; // start muted
+	Morse.osc = context.createOscillator();
+	Morse.osc.type = 'sine';
+	Morse.osc.frequency.value = 440; // Hz
+
+	Morse.osc.connect(Morse.vol);
+	Morse.vol.connect(context.destination);
+	Morse.signal = false;
+	Morse.osc.start();
+
+	// Init queue
+	Morse.bits = new Queue();
+};
+
+// tmp
+Morse.signalOn = function() { Morse.signal = true; }
+Morse.signalOff = function() { Morse.signal = false; }
+
+window.onkeydown = Morse.signalOn;
+window.onkeyup = Morse.signalOff;
+
+Morse.signalPlay = function(durs) {
+	Morse.bits.pushArrays(Morse.getBitstream(durs))
+};
+
+Morse.update = function() {
+	requestAnimationFrame(Morse.update);
+
+	var userInput = false; // tmp
+	if (!userInput) {
+		Morse.signal = Morse.bits.next();
+		console.log(Morse.signal);
+	}
+
+	Morse.vol.gain.value = !!Morse.signal; // set signal
+};
+
+
+Morse.animFrame = requestAnimationFrame(Morse.update);
---
+++ queue.js
@@ -0,0 +1,39 @@
+"use strict";
+
+/* Infinite FIFO queue
+ * Can be used as an infinite bit stream
+ * next() triggers a tick, and will initially only returns zero.
+ * push(val, [n]) pushes `val` to the queue, and will return after `n` ticks after the previous tick
+ */
+var Queue = function(defaultVal) {
+	this.defaultVal = defaultVal || 0;
+	this.vals = [];
+	this.ticks = [];
+};
+
+Queue.prototype.next = function() {
+	if (this.vals.length == 0) { return this.defaultVal; }
+	var a = --this.ticks[this.ticks.length-1]; // Ticks left to return the next value
+	// Pop and return value if zero
+	if (a == 0) {
+		this.ticks.pop();
+		return this.vals.pop();
+	} else return this.defaultVal;
+};
+
+Queue.prototype.push = function(val, n) {
+	n = n || 1;
+	this.vals.push(val);
+	this.ticks.push(n);
+};
+
+Queue.prototype.pushArrays = function(vals, ns) {
+	ns = ns || Array(vals.length).fill(1);
+	this.vals = this.vals.concat(vals.reverse()); // reverse because it's FIFO
+	this.ticks = this.ticks.concat(ns.reverse());
+};
+
+/* TODO: safety check
+ * n is an (+) integer
+ * repeat ns if not same length
+ */
